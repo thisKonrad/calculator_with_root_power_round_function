05d43e85824ed87e4a2c34fc15aa832a32186b64
Konrad von Wittich <127662701+thisKonrad@users.noreply.github.com>
2023-09-25 12:08:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,71 +3,124 @@
  *  functions: √, x², ≈ with Math()Methods ::,
  * */ 
 
-'use strict';
+
+(function(){
 
 /* :: get the result-area :: */
-const resultValue = document.querySelector('#result-area');
+const resultValue = document.querySelector('#result_area');
 
-/* :: # numbers # - add click event to all numbers and evaluable signs with a loop :: */
 
-const numberSigns = document.querySelectorAll('.sign-number');
+/**  add click event to all numbers
+* and evaluable signs with the forEach loop  */
+
+const numberSigns = document.querySelectorAll('.sign');
 
 numberSigns.forEach( numberSign => {
-        numberSign.addEventListener('click', (event)=>{
-        console.log('event');
-        resultValue.innerText += event.target.innerText;
+
+    numberSign.addEventListener('click', (e)=>{
+       
+        resultValue.innerText += e.target.innerText;
+
     })  
+
 });
 
 
-/** = equal :: add here the basic mathematic functions with getResult() */
+/** = equal :: 
+ * add here the basic mathematic functions with getResult() */
 const equal = document.querySelector('#equal');
+
 equal.addEventListener('click', getResult);
 
 function getResult(){
-    /**  eval() Method evaluates the innerText string in a mathematic operation
-    * it only works fine with these operators =► [+, -, *, /,] */
+    /**  eval() Method evaluates the innerText string 
+    * into a mathematic operation
+    * it only works fine with this operators [ +, -, *, / ] */
 
     let result = eval(resultValue.innerText).toFixed(2);
     resultValue.innerText = result;
 
-    /* ensure that if only equal is clicked, undefined wont be displayed */
+    /* ensure that if only equal is clicked, 
+    * undefined wont be displayed */
     if( resultValue.innerText == 'undefined'){
+
         resultValue.innerText = null;
+
     }
+    
 };
 
 
 /* stroke and delete all innerText items with one click */
-resultValue.addEventListener('click',(e)=>{
+resultValue.addEventListener('click',()=>{
+
     resultValue.innerText = null;
+
 });
 
 /* delete last innerText item with DEL Button */
 const del = document.querySelector('#DEL');
-    del.addEventListener('click', (e)=>{
-    resultValue.innerText = resultValue.innerText.slice(0,-1); 
+
+    del.addEventListener('click', ()=>{
+
+    resultValue.innerText = resultValue.innerText.slice(0,-1);
+
 });
 
 
-/* ::▼:: other mathematic functions that does not run so well with eval()Method :: */
+/* ::▼:: other mathematic functions 
+* that does not run with eval()Method :: */
 
 /* '≈' round */
 const round = document.querySelector('#round');
-    round.addEventListener('click', (e)=>{
+
+    round.addEventListener('click', ()=>{
+
     resultValue.innerText = Math.round(resultValue.innerText);
+
 });
 
 /* '√' root */
 const root = document.querySelector('#root');
-root.addEventListener('click',(e)=>{
-     resultValue.innerText = Math.sqrt(resultValue.innerText).toFixed(2);
+root.addEventListener('click',()=>{
+
+    /** check if value is greater than 0 
+    * or you can not calculate the square root*/
+    if( resultValue.innerText > 0){
+
+        const mathRoot = Math.sqrt(
+            resultValue.innerText).toFixed(2);
+
+        resultValue.innerText = `${mathRoot}`;
+    }
+    else{
+
+        resultValue.innerText = 'ERROR';
+
+        setTimeout(() => {
+            
+        resultValue.innerText = null;
+    
+        }, 1000);
+
+    }
+    
 });
+
 
 /* 'x²' power (potenz)  */
-const potenz2 = document.querySelector('#power');
-    potenz2.addEventListener('click', (e)=>{
-    resultValue.innerText = Math.pow(resultValue.innerText, 2).toFixed(2);
+const power2 = document.querySelector('#power');
+
+power2.addEventListener('click', ()=>{
+
+        let squareRoot =  Math.pow(
+            resultValue.innerText, 2).toFixed(2);
+
+            resultValue.innerText = `${squareRoot}`;
+
 });
 
+
 /* :::::: ≡≡≡ :♦: ≡≡≡ :♦: the end :♦: ≡≡≡ :♦: ≡≡≡ ::::: */
+
+}());
